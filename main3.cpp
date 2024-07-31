55ff941580ff9298303e274f2a4a838c88156071
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-07-31 22:44:12 +0800

    commit4

--- main3.cpp
+++ main3.cpp
@@ -1,44 +1,38 @@
 #include <iostream>
 using namespace std;
 
-class character{
+class Animal{
+  protected:
+    const char* mName;
+
   public:
-    virtual void act(character* charptr){
-      cout << "Character acting" << endl;
+    Animal(const char* name): mName(name){
+      cout << "Animal constructor called" << mName << endl;
+    }
+    virtual ~Animal(){
+      cout << "Animal destructor called" << mName << endl;
+    }
+    virtual void  makeSound() const {
+      cout << "Animal sound" << endl;
+    }
+};
+
+class Dog: public Animal{
+  public:
+    Dog(const char* name): Animal(name){
+      cout << "Dog constructor called" << mName << endl;
+    }
+    ~Dog() override {
+      cout << "Dog destructor called" << mName << endl;
+    }
+    virtual void makeSound() const override {
+      cout << "Dog sound" << endl;
     }
 
 };
-
-
-class goblin: public character{
-  public:
-    void act(character* charptr)override{
-      cout << "Goblin acting" << endl;
-    }
-
-};
-
-class dragon : public character{
-  public:
-    void act(character* charptr)override{
-      cout << "Dragon acting" << endl;
-    }
-
-};
-
-
-
-void battle(character* charptr1, character*charpter2){
-  charptr1->act(charpter2);;
-  charpter2->act(charptr1);
-}
-
 
 int main(){
-  goblin goblin1;
-  dragon dragon1;
-
-  battle(&goblin1, &dragon1);
-
+  Dog dog("Buddy");
+  dog.makeSound();
   return 0;
 }
