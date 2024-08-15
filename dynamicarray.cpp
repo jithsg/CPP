53f00f0610ccfed00e9f252bb3b90ea6167e25e6
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-08-15 10:49:29 +0800

    commit10

--- dynamicarray.cpp
+++ dynamicarray.cpp
@@ -1,12 +1,49 @@
-#include <memory>
 #include <iostream>
 using namespace std;
 
-void func(int* ptr){
-  cout << *ptr << endl;
-}
+
+class Sword{
+  public:
+  int mDamage {10};
+  void swing(){
+    cout << "Attacking with sword" << endl;
+  }
+
+};
+
+
+class Player{
+
+  public:
+  Sword* mWeapon {nullptr};
+
+
+  Player(Sword* weapon){
+    mWeapon = weapon;
+  }
+
+  void attack(){
+    mWeapon->swing();
+  }  
+       
+
+};
+
 
 int main(){
-  auto ptr = make_unique<int>(10);
-  func(ptr.get());
+
+  Sword s1;
+
+  Player p1(&s1);
+  p1.attack();
+  p1.mWeapon->mDamage=50;
+  cout << "Damage: " << s1.mDamage << endl;
+
+  Player p2{p1};
+  cout<<p2.mWeapon->mDamage<<endl;
+
+  return 0;
+
+
+
 }
