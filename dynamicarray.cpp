064573fe4c5d6984b9093b48b223223370c43532
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-08-17 02:00:43 +0800

    commit21

--- dynamicarray.cpp
+++ dynamicarray.cpp
@@ -1,10 +1,39 @@
-#include <iostream>
-#include "math_declarations.h"
-
+#include<iostream>
 using namespace std;
 
 
-int main() {
+struct Character{
+  int mHealth;
 
-cout << math::Add(5, 7) << endl;
+  Character(int health): mHealth(health) {cout << "Character constructor" << endl;};
+
+  void PrintHealth(){
+    cout << "Character Health: " << mHealth << endl;
+
+  }
+  ~Character(){cout << "Character destructor" << endl;}
+};
+
+struct Player: public Character{
+  Player(int health): Character(health) {cout << "Player constructor" << endl;};
+
+  void PrintHealth(){
+    cout << "Player Health: " << mHealth << endl;
+  }
+  ~Player(){cout << "Player destructor" << endl;}
+};
+
+enum class Direction{
+  Left, Right, Up, Down
+};
+
+
+
+int main(){
+  Character c(100);
+  c.PrintHealth();
+  Player p(100);
+  p.PrintHealth();
+  Direction dir {Direction::Left};
+  return 0;
 }
