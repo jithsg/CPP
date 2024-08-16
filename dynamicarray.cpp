1f9d949989c1de2557019727133e10608623f74d
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-08-16 23:00:36 +0800

    commit19

--- dynamicarray.cpp
+++ dynamicarray.cpp
@@ -1,24 +1,10 @@
-#include <memory>
 #include <iostream>
+#include "math.cpp"
 using namespace std;
 
-int Add(int x, int y);
-float Add(float x, float y);
+
 
 int main() {
- cout<<Add(1, 2)<<endl;
-cout<<Add(float(1.0f), float(2.0f))<<endl;
-cout<<Add(static_cast<int>(1.0f), static_cast<int>(2.0f))<<endl;
-return 0;
 
-}
-
-
-int Add(int x, int y) {
-  return x + y;
-}
-
-// Float Overload
-float Add(float x, float y) {
-  return x + y;
+cout << Add(5, 7) << endl;
 }
