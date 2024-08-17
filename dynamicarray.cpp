c386aeb9072229ce7a637576a361160485679052
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-08-17 22:27:32 +0800

    commit24

--- dynamicarray.cpp
+++ dynamicarray.cpp
@@ -13,15 +13,36 @@ vector operator+(const vector &a, const vector &b){
   result.y = a.y + b.y;
   result.z = a.z + b.z;
   return result;
- 
+
 }
+
+vector operator-(const vector &a, const vector &b){
+  vector result;
+  result.x = a.x - b.x;
+  result.y = a.y - b.y;
+  result.z = a.z - b.z;
+  return result;
+}
+
+vector operator*(const vector &a, const vector &b){
+  vector result;
+  result.x = a.x * b.x;
+  result.y = a.y * b.y;
+  result.z = a.z * b.z;
+  return result;
+}
+
 
 int main(){
 vector a{1,2,3};
 vector b{4,5,6};
 
 vector c = a + b;
+vector d = a - b;
 cout << c.x << " " << c.y << " " << c.z << endl;
+cout << d.x << " " << d.y << " " << d.z << endl;
+vector e = a * b;
+cout << e.x << " " << e.y << " " << e.z << endl;
 return 0;
 
 }
