939992cbb8f35bd10694ce0bcaca8422b069ddfa
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-08-19 03:25:37 +0800

    commit31

--- smart.cpp
+++ smart.cpp
@@ -9,7 +9,7 @@ class Character{
         int mHealth;
 
         Character(string name, int health): mName(name), mHealth(health){
-            cout<<"Creating "<<mName<<endl;
+       
         }
         ~Character(){
             cout<<"Destroying "<<mName<<endl;
@@ -34,12 +34,18 @@ void display_health(Character* object){
 int main(){
 
     unique_ptr<Character> ptr1 {make_unique<Character>("Player1", 100)};
-    cout<<"Logging "<<ptr1->mName<<endl;
-    display_name(move(ptr1));
+    cout<<ptr1->mName<<endl;
+    // display_name(move(ptr1));
     unique_ptr<Character> ptr2 {make_unique<Character>("Player2", 100)};
-    cout<<"Logging "<<ptr2->mName<<endl;
-    display_name(move(ptr2));
-    
+    cout<<ptr2->mName<<endl;
+    // display_name(move(ptr2));
+    // cout<<ptr1.get()<<endl;
+    // Character* ptr3 {ptr1.release()};
+    // ptr1.reset(new Character("Player2", 100));
+    // cout<<ptr1.get()<<endl;
+    ptr1.swap(ptr2);
+    cout<<ptr1->mName<<endl;
+    cout<<ptr2->mName<<endl;
 
     // ptr1->display();
 
