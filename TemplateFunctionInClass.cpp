f1a5212c5afcd0ce6c39e56a7ba4308eef67de2d
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-08-24 03:41:54 +0800

    commit51

---
+++ TemplateFunctionInClass.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+
+class Monster{
+    public:
+        string mName;
+    };
+    
+
+class Player{
+    public:
+        string mName;
+    template <typename T>
+    void AttackDisplay(T other){
+        cout << this->mName << " Attacking " << other.mName << endl;
+    }
+
+};
+
+int main(){
+    Player p1{"Player1"};
+    Player p2{"Player2"};
+    p1.AttackDisplay<Player>(p2);
+    
+    Monster m1{"Monster1"};
+
+    p1.AttackDisplay<Monster>(m1);
+
+}
