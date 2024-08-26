93df3074342fbf352dfeca345b191b064ab52e58
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-08-27 01:00:48 +0800

    commit65

---
+++ concepts1.cpp
@@ -0,0 +1,35 @@
+#include<concepts>
+#include<iostream>
+using namespace std;    
+
+
+template<typename T>
+class Base {
+    public:
+    T object;
+    Base() {
+        cout<<"Non-Integral template called"<<endl;
+    }
+
+};
+
+template<integral T>
+class Base<T>{
+    public:
+    T object;
+    Base() {
+        cout<<"Integral template called"<<endl;
+    }
+
+};
+
+
+int main() {
+
+    Base<int> b;
+    b.object = 5;
+    cout<<b.object<<endl;
+
+
+    return 0;
+}
