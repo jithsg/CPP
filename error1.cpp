cca0f695f1b2d390a4727bf5b2a8892c552e9a5b
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-08-29 14:39:04 +0800

    commit74

--- error1.cpp
+++ error1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include<type_traits>
-
+#include <cassert>
 using namespace std;
 
 
@@ -29,3 +29,104 @@ using namespace std;
 
 //     return 0;
 // }
+// #include <iostream>
+// #include <cassert>
+
+// class Character {
+// public:
+//   std::string GetName() {
+//     return "Legolas";
+//   };
+// };
+
+// void LogName(Character* Player) {
+//   assert(Player);  
+//   std::cout << Player->GetName();
+// }
+
+// int main() {
+//     Character player;
+//     Character* playerPtr = &player;
+//   LogName(playerPtr);
+// }
+
+// int divide(int x, int y) {
+//     assert(("Denominator must be non-zero", y != 0));
+//     return x / y;
+// }
+
+
+// int main() {
+//     cout << divide(10, 0) << endl;
+//     return 0;
+// // }
+// #include <iostream>
+
+// int Divide(int x, int y) {
+//   if (y == 0)
+//   { throw 0; }
+//   return x/y;
+// }
+
+// int main() {
+//   Divide(5, 0);
+// }
+// #include <iostream>
+// #include <stdexcept>
+
+// double Divide(double a, double b) {
+//     if (b == 0) {
+//         throw std::runtime_error("Division by zero error");
+//     }
+//     return a / b;
+// }
+
+// int main() {
+//     try {
+//         double result = Divide(10.0, 0.0);  // This will cause a division by zero
+//         std::cout << "Result: " << result << std::endl;
+//     } catch (const std::runtime_error& e) {
+//         std::cout << "Caught an exception: " << e.what() << std::endl;
+//     }
+
+//     std::cout << "Program continues..." << std::endl;
+
+//     return 0;
+// }
+// #include <iostream>
+
+// int Divide(int x, int y) {
+//   if (y == 0) throw "Cannot divide by zero";  
+//   return x / y;
+// }
+
+// int main() {
+//   try {
+//     Divide(5, 0);
+//   } catch (...) {
+//     std::cout << "I caught an error";
+//   }
+
+//   std::cout
+//     << "\nThe program can continue as normal";
+// }
+
+
+int Divide(int x, int y){
+    if (y==0) {
+        throw "cannot divide by zero";
+
+    }
+            return x/y;
+}
+
+int main(){
+    try {
+        Divide (5, 0);
+    } 
+    catch(...){
+        cout << "I caught an error";
+    }
+    cout << "\nThe program can continue as normal";
+
+}
