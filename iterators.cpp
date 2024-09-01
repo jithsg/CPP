1cc5ac20d7f4a182e7d0a83691317d6903981b74
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-02 03:15:30 +0800

    commit91

--- iterators.cpp
+++ iterators.cpp
@@ -1,8 +1,24 @@
 #include <array>
 #include <iostream>
+#include <string>
 using namespace std;
+// int main() {
+//   std::array Nums{1, 2, 3};
+//   auto Start{Nums.begin()};
+//   cout << "First element: " << *Start << endl;
+// }
+
+class Character {
+    public:
+        string GetName() {
+            return "Anna" ;
+        }   
+
+};
+
 int main() {
-  std::array Nums{1, 2, 3};
-  auto Start{Nums.begin()};
-  cout << "First element: " << *Start << endl;
+    array<Character, 3> Characters;
+
+    auto Start{Characters.begin()}; 
+    cout<<Start->GetName()<<endl;
 }
