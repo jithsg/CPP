686a507c2cf2829b7d13bfdb086439e059150a99
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-01 12:43:17 +0800

    commit88

---
+++ mdspan.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+
+
+using namespace std;
+
+
+int main(){
+    vector<vector <int>> Grid{
+
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    
+}
