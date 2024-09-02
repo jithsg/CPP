a8da7aa3d7d15e6e0ef5b2dba192e6bf54fded6f
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-02 15:44:43 +0800

    commit92

--- iterators.cpp
+++ iterators.cpp
@@ -1,6 +1,7 @@
 #include <array>
 #include <iostream>
 #include <string>
+#include <forward_list>
 using namespace std;
 // int main() {
 //   std::array Nums{1, 2, 3};
@@ -8,17 +9,26 @@ using namespace std;
 //   cout << "First element: " << *Start << endl;
 // }
 
-class Character {
-    public:
-        string GetName() {
-            return "Anna" ;
-        }   
+// class Character {
+//     public:
+//         string GetName() {
+//             return "Anna" ;
+//         }   
 
-};
+// };
 
-int main() {
-    array<Character, 3> Characters;
+// int main() {
+//     array<Character, 3> Characters;
 
-    auto Start{Characters.begin()}; 
-    cout<<Start->GetName()<<endl;
-}
+//     auto Start{Characters.begin()}; 
+//     cout<<Start->GetName()<<endl;
+// }
+
+// int main(){
+//     forward_list<int> Nums{1,2,3,4,5};
+//     auto Start {Nums.begin()};
+//     auto Second {++Nums.begin()};
+//     cout<<"First: "<<*Start<<endl;
+//     cout<<"Second: "<< *Second<<endl;
+
+// }
