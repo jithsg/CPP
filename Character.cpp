2c7f844e51022f90807d0f0df72785352f2e4098
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-04 02:23:50 +0800

    commit108

--- Character.cpp
+++ Character.cpp
@@ -7,6 +7,7 @@ int main(){
 
     unordered_set <Character> Characters;
     Characters.insert(Character("A"));
+    Characters.emplace("B");
 
     for (auto& item: Characters){
         cout<<item.mName<<endl;
