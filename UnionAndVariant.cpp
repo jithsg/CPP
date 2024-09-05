87c6badd735e0e435408f4524830d468c7890513
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-05 11:02:24 +0800

    commit120

--- UnionAndVariant.cpp
+++ UnionAndVariant.cpp
@@ -6,7 +6,9 @@ union NumberUnion{
     float f;
 };
 
-
+// struct sample{
+//     sample()=delete;
+// };
 
 int main(){
 using std::cout, std::endl, std::string;
@@ -16,13 +18,17 @@ using std::cout, std::endl, std::string;
 // // num.f = 3.14;
 // std::cout<<num.f<<std::endl;
 std::variant<int, float, string> num;
-num = 3;
-cout<<std::get<int>(num)<<endl;
-num = 3.14f;
-cout<<get<float>(num)<<endl;  
+num=3;
+num=3.3f;
+num="hello";
+cout<<std::get<string>(num)<<endl;
+// num = 3;
+// cout<<std::get<sample>(num)<<endl;
+// num = 3.14f;
+// cout<<get<float>(num)<<endl;  
 
-num="Hello";
-cout<<get<string>(num)<<endl;
+// num="Hello";
+// cout<<get<string>(num)<<endl;
 
 
 
