f5f93ec2a6838c0eaff997737c63a8ebac16ca47
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-05 11:49:46 +0800

    commit121

--- UnionAndVariant.cpp
+++ UnionAndVariant.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
 #include <variant>
 
-union NumberUnion{
-    int i;
-    float f;
-};
+// union NumberUnion{
+//     int i;
+//     float f;
+// };
 
 // struct sample{
 //     sample()=delete;
 // };
 
+class Character{
+    public:
+    std::string Name;
+    Character(std::string name):Name(name){}
+};
+
 int main(){
 using std::cout, std::endl, std::string;
-// NumberUnion num;
-// num.i = 42;
-// std::cout<<num.i<<std::endl;
-// // num.f = 3.14;
-// std::cout<<num.f<<std::endl;
-std::variant<int, float, string> num;
-num=3;
-num=3.3f;
-num="hello";
-cout<<std::get<string>(num)<<endl;
+
+std::variant<Character, int> num(Character("Anna"));
+
+num.emplace<0>("Annie");
+
+cout<<std::get<Character>(num).Name<<endl;
+// num=3;
+// num=3.3f;
+// num="hello";
+// cout<<std::get<string>(num)<<endl;
 // num = 3;
 // cout<<std::get<sample>(num)<<endl;
 // num = 3.14f;
