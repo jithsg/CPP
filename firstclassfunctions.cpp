ae375ffd10a9b42d889a0cf14da3c8f8f32f624e
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-09 02:29:25 +0800

    commit134

--- firstclassfunctions.cpp
+++ firstclassfunctions.cpp
@@ -1,28 +1,58 @@
 #include <iostream>
 
-void foo(){
-    std::cout<<"I am a function"<<std::endl;
-}
+// void foo(){
+//     std::cout<<"I am a function"<<std::endl;
+// }
 
-int add(int a, int b){
-    return a+b;
-}
+// int add(int a, int b){
+//     return a+b;
+// }
+
+// int main(){
+// // void (*fptr)()= &foo;
+// // std::cout<<fptr<<std::endl;
+
+// using falias = int (*)(int, int);
+
+// int (*addptr)(int, int) {&add};
+// falias addptr2 {add};
+
+// std::cout<<addptr(1,2)<<std::endl;
+// std::cout<<addptr2(1,2)<<std::endl;
+// addptr=nullptr;
+// if (addptr){
+// std::cout<<(*addptr)(1,2)<<std::endl;
+// }
+
+// }
+
+class Player{
+    public:
+    bool isAlive ()const{
+        return true;
+    }
+};
+
+class Party{
+    public:
+    Player PlayerOne;
+    Player PlayerTwo;
+    Player PlayerThree;
+    
+};
 
 int main(){
-// void (*fptr)()= &foo;
-// std::cout<<fptr<<std::endl;
+    Party MyParty;
+    bool isEveryoneAlive{
+        MyParty.PlayerOne.isAlive() &&
+        MyParty.PlayerTwo.isAlive() &&
+        MyParty.PlayerThree.isAlive()
+    };
 
-using falias = int (*)(int, int);
-
-int (*addptr)(int, int) {&add};
-falias addptr2 {add};
-
-std::cout<<addptr(1,2)<<std::endl;
-std::cout<<addptr2(1,2)<<std::endl;
-addptr=nullptr;
-if (addptr){
-std::cout<<(*addptr)(1,2)<<std::endl;
+    if (isEveryoneAlive){
+        std::cout<<"Everyone is alive"<<std::endl;
+    }
+    else{
+        std::cout<<"Someone is dead"<<std::endl;
+    }
 }
-
-}
-
