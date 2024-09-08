919088f40aa8d648107624929e56a80a087d3874
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-09 00:57:58 +0800

    commit131

--- firstclassfunctions.cpp
+++ firstclassfunctions.cpp
@@ -4,8 +4,16 @@ void foo(){
     std::cout<<"I am a function"<<std::endl;
 }
 
+int add(int a, int b){
+    return a+b;
+}
+
 int main(){
 // void (*fptr)()= &foo;
 // std::cout<<fptr<<std::endl;
+
+int (*addptr)(int, int) {&add};
+
+std::cout<<addptr(1,2)<<std::endl;
 }
 
