48eeeace8d577f7b5527dd095b14b94852e633e3
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-09 01:26:44 +0800

    commit132

--- firstclassfunctions.cpp
+++ firstclassfunctions.cpp
@@ -12,8 +12,12 @@ int main(){
 // void (*fptr)()= &foo;
 // std::cout<<fptr<<std::endl;
 
+using falias = int (*)(int, int);
+
 int (*addptr)(int, int) {&add};
+falias addptr2 {add};
 
 std::cout<<addptr(1,2)<<std::endl;
+std::cout<<addptr2(1,2)<<std::endl;
 }
 
