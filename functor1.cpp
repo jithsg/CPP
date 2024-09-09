4e65eef744f05206275b4da9d581f010d47c5d8f
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-09 15:53:52 +0800

    commit143

--- functor1.cpp
+++ functor1.cpp
@@ -2,16 +2,37 @@
 
 class Func{
     public: 
-    void operator()() const {
+    void operator()() {
         std::cout<<"I am a functor" <<std::endl;
+        ++ state;
+        std::cout<<"My state is " <<state<<std::endl;
+
+
     }
+    int operator()(int x, int y) {
+        std::cout<<"I am a functor returning a sum" <<std::endl;
+        return x+y;
+    }
+    
+    private:
+    int state {0};
 
 };
 
+void isEven(int num, Func func){
+    if (num % 2 == 0){
+        std::cout<<"Even"<<std::endl;
+    }
+    else{
+        std::cout<<"Odd"<<std::endl;
+    }
+  
+}
 
 int main(){
-Func func1;
-func1();
-Func()();
-Func{}();   
+Func func2;
+func2();
+func2();
+func2();
+ 
 }
