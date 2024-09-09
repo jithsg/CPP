b663de434eb288c9ec1546435df0c6a2b7849176
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-09 15:09:43 +0800

    commit141

---
+++ functor1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Func{
+    public: 
+    void operator()() const {
+        std::cout<<"I am a functor" <<std::endl;
+    }
+
+};
+
+
+int main(){
+Func func1;
+func1();
+Func()();
+}
