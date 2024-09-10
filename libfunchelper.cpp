8f25417f900739dfa9c96cadea4116a371d3522a
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-10 20:22:59 +0800

    commit156

--- libfunchelper.cpp
+++ libfunchelper.cpp
@@ -1,29 +1,43 @@
 #include<iostream>
+#include<functional>
+// using Type = void (*)();
+// void Call(std::invocable auto Greet){
+//     Greet();
 
-using Type = void (*)();
+// }
 
+// void Greet(){
+//     std::cout<<"Hello!"<<std::endl;
 
-void Call(auto Greet){
-    Greet();
+// }
+
+// struct Greeter{
+//     public:
+//     void operator()(){
+//         std::cout<<"Hello!"<<std::endl;
+//     }
+// };
+
+// int main(){
+//     Call(Greet);
+//     Call(Greeter());
+//     Call([]{std::cout<<"Hello!"<<std::endl;});
+//     return 0;
+// }
+
+void Call(std::function<void(int, int)> func, int x, int y){
+    func(x, y);
 
 }
 
-void Greet(){
-    std::cout<<"Hello!"<<std::endl;
 
+void func(int x, int y){
+    std::cout<<"x: "<<x<<std::endl;
+    std::cout<<"y: "<<y<<std::endl;
 }
-
-struct Greeter{
-    public:
-    void operator()(){
-        std::cout<<"Hello!"<<std::endl;
-    }
-};
-
 
 int main(){
-    Call(Greet);
-    Call(Greeter());
-    Call([]{std::cout<<"Hello!"<<std::endl;});
+    Call(func, 10, 20);
     return 0;
+
 }
