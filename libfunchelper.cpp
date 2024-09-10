7a97f978afdf50de9e63ac38d8b9bf0e7d2c234e
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-11 01:55:57 +0800

    commit160

--- libfunchelper.cpp
+++ libfunchelper.cpp
@@ -1,6 +1,4 @@
-#include<iostream>
-#include<functional>
-#include<concepts>
+
 // using Type = void (*)();
 // void Call(std::invocable auto Greet){
 //     Greet();
@@ -108,3 +106,44 @@
 //     return 0;
 
 // }
+
+#include<iostream>
+#include<functional>
+#include<concepts>
+class Player{
+    public:
+    bool isOnline() const{
+        return true;
+    }
+};
+
+class Party{
+    private:
+    Player player1;
+    Player player2;
+    Player player3;
+
+    public:
+    bool all_of(std::invocable<const Player&>auto P){
+        return P(player1) && P(player2) && P(player3);
+
+    }
+};
+
+struct OnlineChecker{
+    bool operator()(const Player& player){
+        return player.isOnline();
+    }
+
+};
+
+
+int main(){
+    Party party;
+    if (party.all_of(OnlineChecker())){
+        std::cout<<"All players are online"<<std::endl;
+    }
+    else{
+        std::cout<<"Not all players are online"<<std::endl;
+    };
+}
