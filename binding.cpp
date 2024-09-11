9f167b652da62d2e0031e9f58d1ea1d3a55eceb7
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-11 12:23:56 +0800

    commit165

--- binding.cpp
+++ binding.cpp
@@ -11,9 +11,10 @@ void func(int x, int y, int z){
 int main(){
 
 auto Func_1 {std::bind_front(func, 10, 20)};
-
+auto Func_2 {std::bind(func, 10, std::placeholders::_1, std::placeholders::_2)};
 Func_1(30);
 Func_1(40);
 Func_1(50);
+Func_2(30, 40);
 return 0;
 }
