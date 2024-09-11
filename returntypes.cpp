5ead8c4d6ccd9adaf2f1831bd604b0d4338b27f4
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-11 23:40:35 +0800

    commit170

--- returntypes.cpp
+++ returntypes.cpp
@@ -2,22 +2,22 @@
 #include<string>
 #include<vector>
 
-template<typename T1, typename T2>
-auto Multiply(T1 x, T2 y)->decltype(x*y){
-    if (x==0 || y==0) return 0;
+// template<typename T1, typename T2>
+// auto Multiply(T1 x, T2 y)->decltype(x*y){
+//     if (x==0 || y==0) return 0;
 
-    return x*y;
-}
+//     return x*y;
+// }
 
-int main(){
-    auto result {Multiply(10.0, 0.0)};
-    auto result1 {Multiply(10, 0)};
-    std::cout<<result<<std::endl;
-    std::cout<<typeid(result).name()<<std::endl;
-    std::cout<<typeid(result1).name()<<std::endl;
-    std::cout<<typeid(Multiply(10.5f, 20.5f)).name()<<std::endl;
-    return 0;
-}
+// int main(){
+//     auto result {Multiply(10.0, 0.0)};
+//     auto result1 {Multiply(10, 0)};
+//     std::cout<<result<<std::endl;
+//     std::cout<<typeid(result).name()<<std::endl;
+//     std::cout<<typeid(result1).name()<<std::endl;
+//     std::cout<<typeid(Multiply(10.5f, 20.5f)).name()<<std::endl;
+//     return 0;
+// }
 
 // int main(){
 //     int x= 5;
@@ -29,3 +29,47 @@ int main(){
 // auto Multiply(int x, int y){
 //     return decltype(x*y) {x*y};
 // }
+
+// class Player{
+//     public:
+//     static const int mHealth {100};
+// };
+
+
+// int main(){
+//     std::cout<<Player::mHealth<<std::endl;
+//     return 0;
+// }
+
+template<int R, int C>
+class Matrix{
+    public:
+    static const int Rows {R};
+    static const int Cols {C};
+
+    Matrix(){};
+    Matrix(int){};
+
+
+    bool operator==(int){return true;}
+
+    auto operator*(auto Other){
+        return Matrix<Rows, Other.Cols>{};
+    }
+};
+
+template <typename T1, typename T2>
+auto Multiply(T1 x, T2 y)->decltype(x*y){
+    if (x==0 || y==0) return 0;
+    return x*y;
+}
+
+int main(){
+  Matrix<3, 1> A;
+  Matrix<1, 3> B;
+  auto Result{Multiply(A, B)};
+
+  std::cout
+    << "Rows: " << Result.Rows
+    << "\nColumns: " << Result.Cols;
+}
