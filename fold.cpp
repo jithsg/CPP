1e2173d62bea44a9a1cb4fe588704924e3383738
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-12 14:58:08 +0800

    commit176

--- fold.cpp
+++ fold.cpp
@@ -21,3 +21,44 @@
 //     std::cout << fold(1, 2, 3, 4, 5) << std::endl;
 //     return 0;
 // }
+
+
+
+
+// template<typename ...Types>
+// void fold(Types ...args){
+//     (
+//         [&](){std::cout << args << std::endl;}(),...);
+// }
+
+// int main(){
+//     fold(1, 2, 3, 4, 5);
+//     return 0;
+// }
+
+
+// template<typename ...Types>
+// void rightfold(Types...Args){
+//     std::cout<<(Args-...-0)<<std::endl;
+// }
+
+// template<typename ...Types>
+// void leftfold(Types...Args){
+//     std::cout<<(0-...-Args)<<std::endl;
+// }
+
+// int main(){
+//     leftfold(1, 2, 3, 4, 5);
+//     rightfold(1, 2, 3, 4, 5);
+//     return 0;
+// }
+
+template<typename ...Types>
+void fold(Types ...args){
+    (std::cout<<...<<args)<<std::endl;
+}
+
+int main(){
+    fold(1, 2, 3, 4, 5);
+    return 0;
+}
