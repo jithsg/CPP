34be4f3f77a71caeb98de4c0693a0cf8bc2efdca
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-12 11:41:48 +0800

    commit175

---
+++ fold.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+// template <typename T>
+// T Log(T x){
+//     std::cout << "arg: " << x << std::endl;
+//     return x;
+
+// }
+
+
+
+// template <typename ...Types>
+// auto fold(Types ...args){
+//     return (Log(args) +...);
+
+// }
+
+
+
+// int main(){
+//     std::cout << fold(1, 2, 3, 4, 5) << std::endl;
+//     return 0;
+// }
