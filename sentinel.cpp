230bb3f69a30b8e173c5f55c7c908e755b854110
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-13 02:53:30 +0800

    commit180

--- sentinel.cpp
+++ sentinel.cpp
@@ -4,18 +4,38 @@
 #include<algorithm>
 
 
+// int main(){
+//     std::vector R {1, 2, 3, 4, 5};
+
+//     auto sentinel {R.end()};
+
+//     // for (auto it {R.begin()}; it != sentinel; ++it){
+//     //     std::cout<<*it<<std::endl;
+//     // }
+
+//     std::ranges::for_each(R.begin(), R.end(), [](auto& element){
+//         std::cout<<element+1<<std::endl;
+//     });
+
+
+// }
+
+struct Sentinel{
+    public:
+    bool operator==(auto it)const{
+        return *it<0;
+    }
+
+
+};
+
 int main(){
-    std::vector R {1, 2, 3, 4, 5};
+    std::vector R {1, 2, 3, -4, 5};
 
-    auto sentinel {R.end()};
+    Sentinel s;
 
-    // for (auto it {R.begin()}; it != sentinel; ++it){
-    //     std::cout<<*it<<std::endl;
-    // }
+    for (auto it {R.begin()}; it != s; ++it){
+        std::cout<<*it<<std::endl;
+    }
 
-    std::ranges::for_each(R, [](auto& element){
-        std::cout<<element+1<<std::endl;
-    });
-
-
-}
+    };
