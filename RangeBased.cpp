6050f71dc471438f91c063b0ade45f46a3f5a810
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-15 03:16:34 +0800

    commit187

--- RangeBased.cpp
+++ RangeBased.cpp
@@ -25,10 +25,33 @@
 //     }
 // }
 
-int main(){
-    std::forward_list<int>numbers = {5,3,2,4,1};
-    numbers.sort();
-    for (auto& number:numbers){
-        std::cout<<number<<" ";
+// int main(){
+//     std::forward_list<int>numbers = {5,3,2,4,1};
+//     numbers.sort();
+//     for (auto& number:numbers){
+//         std::cout<<number<<" ";
+//     }
+// }
+
+struct SomeType{
+
+    int mX;
+
+    SomeType(int x):mX(x){}
+
+    auto operator<=>(const SomeType& other)const{
+        return mX <=> other.mX;
     }
+    bool operator== (const SomeType& other)const{
+        return mX == other.mX;}
+};
+int main(){
+
+    std::vector<SomeType> number{0, 2, 1, 3};
+
+    std::ranges::sort(number);
+    for (auto& number:number){
+        std::cout<<number.mX<<" ";
+    }
+    
 }
