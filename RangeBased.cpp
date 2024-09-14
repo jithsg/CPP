2d2d10cf501b5b297c39a7871d3b931c0f44bfef
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-15 01:59:08 +0800

    commit184

--- RangeBased.cpp
+++ RangeBased.cpp
@@ -2,16 +2,24 @@
 #include <vector>
 #include <algorithm>
 
+// int main(){
+//     std::vector<int> numbers = {5,2,3,4,1};
+
+//     std::ranges::sort(numbers.begin(),numbers.end());
+
+//     for (const auto& number:numbers){
+//         std::cout<<number<<" ";
+//     }
+
+
+
+// }
+
 int main(){
-    std::vector<int> numbers = {5,2,3,4,1};
+    std::vector<int> numbers = {5,3,2,4,1};
+    std::sort(std::next(numbers.begin(), 1),std::prev(numbers.end(), 1)); 
 
-    std::ranges::sort(numbers.begin(),numbers.end());
-
-    for (const auto& number:numbers){
+    for (auto& number:numbers){
         std::cout<<number<<" ";
     }
-
-
-
 }
-
