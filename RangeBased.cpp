38cb1c387b882b5078130874bf7603d0321fc6ab
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-15 02:07:13 +0800

    commit185

--- RangeBased.cpp
+++ RangeBased.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include<forward_list>
 
 // int main(){
 //     std::vector<int> numbers = {5,2,3,4,1};
@@ -15,10 +16,18 @@
 
 // }
 
-int main(){
-    std::vector<int> numbers = {5,3,2,4,1};
-    std::sort(std::next(numbers.begin(), 1),std::prev(numbers.end(), 1)); 
+// int main(){
+//     std::vector<int> numbers = {5,3,2,4,1};
+//     std::ranges::sort(std::next(numbers.begin(), 1),std::prev(numbers.end(), 1)); 
 
+//     for (auto& number:numbers){
+//         std::cout<<number<<" ";
+//     }
+// }
+
+int main(){
+    std::forward_list<int>numbers = {5,3,2,4,1};
+    numbers.sort();
     for (auto& number:numbers){
         std::cout<<number<<" ";
     }
