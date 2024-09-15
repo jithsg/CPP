ebb4a8a25ba31ba48f176001fdf7cd24f39eed59
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-16 01:28:40 +0800

    commit191

--- view.cpp
+++ view.cpp
@@ -1,14 +1,25 @@
 #include<iostream>
 #include<ranges>
 #include<vector>
+#include<algorithm>
 
 int main(){
-    std::vector<int> numbers = {1,2,3,4,5};
+    std::vector<int> numbers = {4, 1, 3, 2, 5};
 
-    auto View {std::views::take(numbers,3)};
+    // auto View {std::views::take(numbers,3)};
 
-    for (const auto& number:View){
-        std::cout<<number<<" ";
+    // for (const auto& number:View){
+    //     std::cout<<number<<std::endl;
+    // }
+
+    // for (const auto& number:std::views::reverse(numbers)){
+    //     std::cout<<number<<std::endl;
+    // }
+
+    std::ranges::sort(std::views::take(numbers,3));
+
+    for (const auto& number:numbers){
+        std::cout<<number<<std::endl;
     }
 
 }
