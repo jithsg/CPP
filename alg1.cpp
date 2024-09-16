90da7efc112f65cb8ffe07dc0982a5d466183edd
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-16 15:28:40 +0800

    commit197

--- alg1.cpp
+++ alg1.cpp
@@ -2,25 +2,32 @@
 #include<algorithm>
 #include<vector>
 
-int proj_add(int& i){
-    return i+=1;
+// int proj_add(int& i){
+//     return i+=1;
+// }
+
+// int disp(int i){
+//     return i;
+// }
+
+class Character{
+    public:
+    std::string mName;
+    Character(std::string name):mName(name){}
+};
+
+std::string disp(Character& ch){
+    return ch.mName;
 }
 
-int disp(int i){
-    return i;
+void log(std::string s){
+    std::cout<<s<<std::endl;
 }
 int main(){
-    std::vector<int> v={1,2,3,4,5};
-
-    // std::ranges::for_each(v, [](int i){std::cout<<i<<" ";});
+std::vector<Character> characters {Character("A"), Character("B"), Character("C")};
 
 
-
-    std::ranges::for_each(v, disp, proj_add);
-    for (int i : v) {
-        std::cout << i << ' ';
-    }
-
+    std::ranges::for_each(characters, log, disp);
     return 0;
 
 }
