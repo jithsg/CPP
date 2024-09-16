8d1d2b4170587140ed3304ea72502f3a2a24cb77
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-16 14:53:27 +0800

    commit196

--- alg1.cpp
+++ alg1.cpp
@@ -2,12 +2,25 @@
 #include<algorithm>
 #include<vector>
 
+int proj_add(int& i){
+    return i+=1;
+}
 
+int disp(int i){
+    return i;
+}
 int main(){
     std::vector<int> v={1,2,3,4,5};
 
     // std::ranges::for_each(v, [](int i){std::cout<<i<<" ";});
 
-    std::ranges::for_each(v, [](int i){std::cout<<i+1<<" ";});
+
+
+    std::ranges::for_each(v, disp, proj_add);
+    for (int i : v) {
+        std::cout << i << ' ';
+    }
+
+    return 0;
 
 }
