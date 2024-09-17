913cd16c6ec081a0d724fa6bcbc20d133d974d33
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-17 16:28:51 +0800

    commit201

--- parallel.cpp
+++ parallel.cpp
@@ -3,15 +3,20 @@
 #include <vector>
 #include<thread>
 #include<chrono>
+#include<execution>
+
 void Log(int Number){
   using namespace std::chrono_literals;
   std::this_thread::sleep_for(1s);
+
+  // There is now a bug here - see explanation below
   std::cout << "Number: " << Number << '\n';
 }
 
 int main(){
   std::vector Numbers{1, 2, 3};
 
-  std::for_each(Numbers.begin(), Numbers.end(),
+  std::for_each(std::execution::par,
+                Numbers.begin(), Numbers.end(),
                 Log);
 }
