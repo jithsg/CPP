d5facd9e97568e0f0a5491476193822eda2b9161
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-17 16:21:00 +0800

    commit199

--- alg1.cpp
+++ alg1.cpp
@@ -33,12 +33,44 @@
 
 // }
 
+// void log(int x){
+//     std::cout<<x<<std::endl;
+// }
+
+// int main(){
+//     std::vector<int> v {1,2,3,4,5}; 
+
+//     std::ranges::for_each(std::views::reverse(v), log);
+// }
+
+#include <iostream>
+#include <vector>
+#include <ranges>
+
+// int main() {
+//     std::vector<int> numbers{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+//     // Create a view that filters even numbers
+//     auto even_numbers = numbers | std::views::filter([](int x) { return x % 2 == 0; });
+
+//     std::cout << "Even Numbers: ";
+//     for (int num : even_numbers) {
+//         std::cout << num << ' ';
+//     }
+//     std::cout << '\n';
+//     std::cout<<typeid(even_numbers).name()<<std::endl;
+//     return 0;
+// }
+
 void log(int x){
     std::cout<<x<<std::endl;
 }
 
 int main(){
-    std::vector<int> v {1,2,3,4,5}; 
+    std::vector<int> v {1,2,3,4,5,6,7};
 
-    std::ranges::for_each(std::views::reverse(v), log);
+    // std::ranges::for_each(v|std::views::filter([](int x){return x%2==0;})|std::views::transform([](int x){return x*2;}), log);
+    std::ranges::for_each(v.begin(), v.end(), log);
+
+    
 }
