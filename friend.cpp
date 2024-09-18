b328ce121a8a94f6b7b66cedd8a74d2833870f61
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-18 19:25:56 +0800

    commit211

--- friend.cpp
+++ friend.cpp
@@ -1,29 +1,49 @@
 #include <iostream>
 
+// class MyClass{
+
+//     friend void LogCall(MyClass);
+//     public:
+//     void operator()(){
+//         Calls++;
+
+//     }
+
+//     private:
+//     int Calls{0};
+
+// };
+
+// void LogCall(MyClass obj){
+//     std::cout<<"Calls made: "<<obj.Calls<<std::endl;
+// }
+
+
+// int main(){
+//     MyClass obj;
+//     obj();
+//     obj();
+//     obj();
+//     LogCall(obj);
+//     return 0;
+// }
+
 class MyClass{
 
-    friend void LogCall(MyClass);
+    friend void showPrivateData(MyClass&);
     public:
-    void operator()(){
-        Calls++;
-
-    }
+    MyClass(int val):mVal(val){}
 
     private:
-    int Calls{0};
-
+    int mVal;
 };
 
-void LogCall(MyClass obj){
-    std::cout<<"Calls made: "<<obj.Calls<<std::endl;
+void showPrivateData(MyClass& obj){
+    std::cout<<"Private data: "<<obj.mVal<<std::endl;
 }
 
-
 int main(){
-    MyClass obj;
-    obj();
-    obj();
-    obj();
-    LogCall(obj);
+    MyClass obj(10);
+    showPrivateData(obj);
     return 0;
 }
