0d8d4e337dab8d06f8f85144e6f80b0b6d496f5d
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-20 00:27:20 +0800

    commit 221

--- ListInitialization.cpp
+++ ListInitialization.cpp
@@ -2,7 +2,7 @@
 
 struct Vec3{
 
-    Vec3(int x, int y, int z):x(x), y(y), z(z){}
+    // Vec3(int x, int y, int z):x(x), y(y), z(z){}
 
     void log(){
         std::cout<<"x: "<<x<<std::endl;
@@ -16,8 +16,17 @@ struct Vec3{
 
 };
 
-int main(){
-    Vec3 v {1,2,3};
+Vec3 getvector(){
+    return {1,2,3};
+}
+
+void HandleVector(Vec3 v){
     v.log();
+}
+int main(){
+    Vec3 v{.x=1, .z=3};
+    // v.log();
+    // getvector().log();
+    HandleVector(v);
     return 0;
 }
