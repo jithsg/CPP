fbb7a83a3ea047f2e6447476309fcc907cd1c395
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-09-19 16:35:11 +0800

    commit216

--- multiple.cpp
+++ multiple.cpp
@@ -1,26 +1,63 @@
 #include<iostream>
 
-class Human{
+// class Human{
+//     public:
+//     int agility{8};
+// };
+
+// class Elf{
+//     public:
+//     int agility{10};
+// };
+
+// class HalfElf: public Human, public Elf{};
+
+
+// int main(){
+
+//     HalfElf elrond;
+
+//     std::cout<<"Elrond's agility (human): "<<elrond.Human::agility<<std::endl;
+//     std::cout<<"Elrond's agility (elf): "<<elrond.Elf::agility<<std::endl;
+// }
+
+
+class Character{
     public:
-    int agility{8};
+    // Character():mAgility(0){};
+    int mAgility;
+    Character(int agility):mAgility(agility){}
 };
 
-class Elf{
+class Human: virtual public Character{
     public:
-    int agility{10};
+    Human():Character(8){}
 };
 
-class HalfElf: public Human, public Elf{};
+class Elf: virtual public Character{
+    public:
+    Elf():Character(10){}
+};
+
+class HalfElf: public Human, public Elf{
+    public:
+    HalfElf():Character(0){}
+};
 
 
 int main(){
 
+    // HalfElf elrond;
+
+    // std::cout<<"Elrond's agility (human): "<<elrond.Human::mAgility<<std::endl;
+    // std::cout<<"Elrond's agility (elf): "<<elrond.Elf::mAgility<<std::endl;
+
+    // Human Aragorn;
+    // Elf Legolas;
+
+    // std::cout<<"Aragorn's agility: "<<Aragorn.mAgility<<std::endl;
+    // std::cout<<"Legolas's agility: "<<Legolas.mAgility<<std::endl;
+
     HalfElf elrond;
-
-    std::cout<<"Elrond's agility (human): "<<elrond.Human::agility<<std::endl;
-    std::cout<<"Elrond's agility (elf): "<<elrond.Elf::agility<<std::endl;
-
-
-
+    std::cout<<"Elrond's agility: "<<elrond.mAgility<<std::endl;
 }
-
